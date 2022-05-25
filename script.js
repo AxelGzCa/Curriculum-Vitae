12b582cdf7c7f4edc659c3e570bca7bcc1fb46f0
AxelGzCa <2005045@utrivieramaya.edu.mx>
2022-05-25 17:20:42 -0500

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if('serviceworker' in navigator){
-    navigator.serviceworker.register('https://axelgzca.github.io/Curriculum-Vitae/serviceworker.js')
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('https://axelgzca.github.io/Curriculum-Vitae/serviceworker.js')
     .then(reg => console.log('Registro sw exitoso', reg))
     .catch(error => console.warn(error))
 }
